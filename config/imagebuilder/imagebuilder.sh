16b95dfab4a7df995c58d7151669024a6e5886db
Milo <107900975+JJ-Coffee@users.noreply.github.com>
2025-05-28 00:49:45 +0700

    Update imagebuilder.sh

--- config/imagebuilder/imagebuilder.sh
+++ config/imagebuilder/imagebuilder.sh
@@ -214,6 +214,7 @@ rebuild_firmware() {
         kmod-usb-net-huawei-cdc-ncm kmod-usb-serial kmod-usb-serial-option kmod-usb-serial-wwan usbutils \
         kmod-usb-net-asix kmod-usb-net-asix-ax88179 kmod-usb-net-dm9601-ether kmod-usb-net-rtl8152 \
         \
+        kmod-rtl8188fu rtl8188fu-firmware \
         \
         ${config_list} \
         "
